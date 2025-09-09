c5393f4d53ad3a64ccf3684077212ac6bf930f5a
Moiz Chauhan <moizrafiq14@gmail.com>
2025-09-09 14:15:44 +0500

    Update PCControl.sv

--- Single Cycle RISC V/PCControl.sv
+++ Single Cycle RISC V/PCControl.sv
@@ -10,61 +10,29 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-//module PCControl(
-//    output logic [31:0] PC_N, PC_Store,
-//    input logic [31:0] aluOut, PC, imm,
-//    input logic [1:0] jump //2 bit jump for deciding between jal, jalr/branch or pc+4
-//    );
-//    always_comb begin
-//        if(jump == 2'b01) begin //JAL or B-TYPE
-//            PC_N = PC + imm;
-//            PC_Store = PC + 4;
-//        end
-//        else if(jump == 2'b11) begin //JALR
-//            PC_N = PC + aluOut;
-//            PC_Store = PC + 4;
-//        end
-//        else if(jump == 2'b10) begin //AUIPC
-//            PC_N = PC + 4;
-//            PC_Store = PC + imm;
-//        end
-//        else begin //no jump
-//            PC_N = PC + 4;
-//            PC_Store = PC + 4;
-//        end    
-//    end
-//endmodule
-
 module PCControl(
-    output logic [31:0] PC_N, PC_Store,
+    output logic [31:0] PC_N,
     input logic [31:0] aluOut, PC, imm,
     input logic [2:0] func3,
-    input logic [6:0] opcode,
-    input logic [1:0] jump, //2 bit jump for deciding between jal, jalr/branch or pc+4
+    input logic [6:0] opcode, 
     input logic carry, zero, negative, overflow //flags
     );
     always_comb begin
         if(opcode != 99)begin
-            if(jump == 2'b01) begin //JAL or B-TYPE
+            if(opcode == 111) begin //JAL or B-TYPE
                 PC_N = PC + imm;
-                PC_Store = PC + 4;
             end
-            else if(jump == 2'b11) begin //JALR
+            else if(opcode == 103) begin //JALR
                 PC_N = PC + aluOut;
-                PC_Store = PC + 4;
             end
-            else if(jump == 2'b10) begin //AUIPC
+            else if(opcode == 23) begin //AUIPC
                 PC_N = PC + 4;
-                PC_Store = PC + imm;
             end
             else begin //no jump
                 PC_N = PC + 4;
-                PC_Store = PC + 4;
             end    
         end
         else begin
-            PC_Store = 32'b00000000;
             case(func3)
                  3'b000: PC_N = zero ? (PC + imm) : (PC + 4); //beq
                  3'b001: PC_N = (!zero) ? (PC + imm) : (PC + 4); //bne
@@ -77,3 +45,4 @@ module PCControl(
         end
     end
 endmodule
+
