e357b6037584c38da2170a346f54376736daba34
Moiz Chauhan <moizrafiq14@gmail.com>
2025-09-17 10:44:21 +0500

    Update control.sv

--- Pipelined RISC V/control.sv
+++ Pipelined RISC V/control.sv
@@ -1,19 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
 // 
 // Create Date: 08/27/2025 04:44:39 PM
-// Design Name: 
+// Design Name: Pipelined RISC V
 // Module Name: control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
+// Project Name: Pipelined RISC V
+// Description: The main control Units for generating Control Signals
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
