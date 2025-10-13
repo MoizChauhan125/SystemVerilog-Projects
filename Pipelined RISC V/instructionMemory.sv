a1ccb197222298e45369bf6d1dadd07a50868fb6
Moiz Chauhan <moizrafiq14@gmail.com>
2025-10-13 18:10:49 +0500

    Update instructionMemory.sv

--- Pipelined RISC V/instructionMemory.sv
+++ Pipelined RISC V/instructionMemory.sv
@@ -19,11 +19,5 @@ module instructionMemory(
     input logic [31:0] addr
     );
     logic [7:0] instructionMem [0:511]; //512 words memory
-    initial begin
-        integer i;
-        for (i = 0; i < 512; i = i + 1) begin
-            instructionMem[i] = 8'h00000013;  // opcode of NOP Instructions
-        end
-    end
     assign instr = {instructionMem[addr+3], instructionMem[addr+2], instructionMem[addr+1], instructionMem[addr]};
 endmodule
